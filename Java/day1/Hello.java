0e891e5e58208fb191b136cc9a2575810eac3e17
Fullstack.jang <Fullstack.jang@gmail.com>
2020-10-21 22:12:20 +0900

    day1 add

---
+++ Java/day1/Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+
+	public static void main(String[] args){
+		
+		// main method: Hello 클래스를 단독으로 실행 시킬 때 main method를 정의한다.
+		// Java Application
+
+		System.out.println("안녕하세요");
+
+	}//main method
+}//class
